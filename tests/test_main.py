f6b028f9e8e9eacf63bc2f88ca6721fb56482e26
Gonzalo Privitera <gonzalo.privitera1@ibm.com>
2025-06-20 18:45:36 -0300

    correccion de codigo se agrega parentesis en una linea

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Añadir src al path para importaciones
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 
 from main import app as flask_app
