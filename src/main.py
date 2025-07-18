9965823c59c8bac94f70021d54bd1067728d3991
Gonzalo Privitera <gonzalo.privitera1@ibm.com>
2025-07-18 16:42:05 -0300

    se agrega webhook render

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 import sentry_sdk
+import random
 from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.exceptions import HTTPException
 
